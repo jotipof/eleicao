cf3e8e21326eefe93a10217f7b6f496b1db2f6e5
JoaoTiago07 <joaotiagopff2007@gmail.com>
2024-11-02 10:04:24 +0000

    commit2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Configuração do Supabase
 const SUPABASE_URL = "https://rbeujdwjajzrlpnbrbaf.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiZXVqZHdqYWp6cmxwbmJyYmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1NDA3NzIsImV4cCI6MjA0NjExNjc3Mn0.QqQS6-QjP6QCsMuMeHOj28UbWu6We0vbzYr9g8AMrn8";
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Buscar e atualizar os dados
 async function buscarVotos() {
