60d18ba9ffad0a3a2e9b0593a8f415015dc36182
JoaoTiago07 <joaotiagopff2007@gmail.com>
2024-11-02 11:45:27 +0000

    asdawdawd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Inicialização do Supabase
 const SUPABASE_URL = "https://rbeujdwjajzrlpnbrbaf.supabase.co"; // substitua pelo valor correto
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiZXVqZHdqYWp6cmxwbmJyYmFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA1NDA3NzIsImV4cCI6MjA0NjExNjc3Mn0.QqQS6-QjP6QCsMuMeHOj28UbWu6We0vbzYr9g8AMrn8"; // substitua pelo valor correto
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY); // Renomeado para evitar conflito
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY); // Renomeado para evitar conflito
 
 // Função para atualizar o gráfico
 function atualizarGrafico(votosData) {
@@ -106,7 +106,7 @@ function preencherMandatos(mandatos) {
 // Função para buscar os dados inicialmente
 async function buscarVotos() {
     console.log("Buscando dados da tabela 'votos'");
-    const { data, error } = await supabaseClient
+    const { data, error } = await supabase
         .from('votos')
         .select('*');
 
@@ -127,7 +127,7 @@ async function buscarVotos() {
 }
 
 // Assinatura para escutar mudanças na tabela
-const votosSubscription = supabaseClient
+const votosSubscription = supabase
     .from('votos')
     .on('*', payload => {
         console.log('Alteração detectada:', payload);
